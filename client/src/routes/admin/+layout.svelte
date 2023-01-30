c8fd59f5674bcc3b9d62a20f9f4a069a9eddc07d
Methapon2001 <dark.methapon13@gmail.com>
2023-01-30 16:10:47 +0700

    update(client): layout

---
+++ client/src/routes/admin/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Sidebar from '$lib/components/Sidebar.svelte';
+</script>
+
+<div class="admin-main">
+  <div class="sidebar bg-slate-500">
+    <Sidebar />
+  </div>
+  <div class="content">
+    <slot />
+  </div>
+</div>=
---
+++ client/src/routes/admin/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { fly } from 'svelte/transition';
+</script>
+
+<div in:fly={{ y: 32, duration: 500 }}>
+</div>
