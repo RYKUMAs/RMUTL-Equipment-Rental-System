96a9c28600800ebe5a5bd5b20b50201953567cf7
Methapon2001 <dark.methapon13@gmail.com>
2023-02-03 17:16:47 +0700

    dynamic to logged in or not

--- client/src/lib/components/Navbar.svelte
+++ client/src/lib/components/Navbar.svelte
@@ -1,14 +1,42 @@
 <script>
+  import { userStore } from '../store';
   import rmutl from '../assets/rmutl.jpg';
 
-  export let data = [];
+  let list = [
+    {
+      name: 'Home',
+      link: '/'
+    }
+  ];
 </script>
 
-<nav class="flex p-3 bg-amber-800 text-white">
-  <img alt="gallery" class="object-contain h-10 w-10 mr-3 place-self-center" src={rmutl} />
-  {#each data as { name, link }}
-    <a href={link} class="inline-block p-2 rounded transition-all hover:bg-amber-700 uppercase"
-      >{name}</a
-    >
-  {/each}
+<nav class="flex p-3 bg-amber-800 text-white justify-between">
+  <div class="flex">
+    <img alt="gallery" class="object-contain h-10 w-10 mr-3 place-self-center" src={rmutl} />
+    {#each list as { name, link }}
+      <a href={link} class="inline-block p-2 rounded transition-all hover:bg-amber-700 uppercase"
+        >{name}</a
+      >
+    {/each}
+  </div>
+  <div>
+    {#if $userStore == null}
+      <a
+        href="/sign-in"
+        class="inline-block p-2 rounded transition-all hover:bg-amber-700 uppercase">Sign In</a
+      >
+      <a
+        href="/sign-up"
+        class="inline-block p-2 rounded transition-all hover:bg-amber-700 uppercase">Sign Up</a
+      >
+    {:else}
+      <span class="inline-block p-2 rounded transition-all hover:bg-amber-700 uppercase"
+        >{$userStore.username}</span
+      >
+      <a
+        class="inline-block p-2 rounded transition-all hover:bg-amber-700 uppercase"
+        href="sign-out">Sign Out</a
+      >
+    {/if}
+  </div>
 </nav>
