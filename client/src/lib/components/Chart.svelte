227b3f58338e327cfeebd8e0e5c852bd84c491aa
Methapon2001 <dark.methapon13@gmail.com>
2023-02-06 15:34:17 +0700

    example chart

---
+++ client/src/lib/components/Chart.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount } from 'svelte';
+
+  function createChart() {
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December'
+        ],
+        datasets: [
+          {
+            label: '# Rent rate',
+            data: [12, 58, 38, 5, 29, 33, 16, 33, 28, 22, 36, 5],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  onMount(createChart);
+</script>
+
+<div>
+  <canvas id="myChart" />
+</div>
--- client/src/routes/admin/+page.svelte
+++ client/src/routes/admin/+page.svelte
@@ -1,6 +1,29 @@
 <script>
   import { fly } from 'svelte/transition';
+  import Chart from '$lib/components/Chart.svelte';
+  import ProfileBox from '$lib/components/ProfileBox.svelte';
 </script>
 
-<div in:fly={{ y: 32, duration: 500 }}>
+<div in:fly={{ y: 32, duration: 500 }} />
+
+<div class="ml-5 mt-5  rounded-lg overflow-hidden border border-slate-500 w-3/5 h-fit">
+  <table class="w-full">
+    <tr class="my-8 border-b bg-slate-500 text-white">
+      <th>Profile</th>
+    </tr>
+    <ProfileBox />
+  </table>
 </div>
+
+<!-- Chart.js -->
+<div class="ml-5 mt-5  rounded-lg overflow-hidden border border-slate-500 w-3/5 h-fit">
+  <table class="w-full">
+    <tr class="my-8 border-b bg-slate-500 text-white">
+      <th>Chart</th>
+    </tr>
+    <div class="ml-2 mt-2 pr-4 content-center  w-auto h-auto">
+      <Chart />
+    </div>
+  </table>
+</div>
+<!-- Chart -->
