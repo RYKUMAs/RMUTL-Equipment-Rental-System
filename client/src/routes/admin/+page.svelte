e18e9551c2f4db58d3e33a4155840b7a646fa746
Methapon2001 <dark.methapon13@gmail.com>
2023-02-07 10:21:58 +0700

    fix: usage chartjs

--- client/src/routes/admin/+page.svelte
+++ client/src/routes/admin/+page.svelte
@@ -4,26 +4,11 @@
   import ProfileBox from '$lib/components/ProfileBox.svelte';
 </script>
 
-<div in:fly={{ y: 32, duration: 500 }} />
-
-<div class="ml-5 mt-5  rounded-lg overflow-hidden border border-slate-500 w-3/5 h-fit">
-  <table class="w-full">
-    <tr class="my-8 border-b bg-slate-500 text-white">
-      <th>Profile</th>
-    </tr>
-    <ProfileBox />
-  </table>
-</div>
-
-<!-- Chart.js -->
-<div class="ml-5 mt-5  rounded-lg overflow-hidden border border-slate-500 w-3/5 h-fit">
-  <table class="w-full">
-    <tr class="my-8 border-b bg-slate-500 text-white">
-      <th>Chart</th>
-    </tr>
-    <div class="ml-2 mt-2 pr-4 content-center  w-auto h-auto">
-      <Chart />
+<div in:fly={{ y: 32, duration: 500 }} class="p-5">
+  <div class="rounded-lg overflow-hidden border border-slate-500">
+    <div class="bg-slate-500 text-white p-3 font-bold text-center">
+      <h1>Chart</h1>
     </div>
-  </table>
+    <Chart />
+  </div>
 </div>
-<!-- Chart -->
